06015e17da243896ba91ae103cea121a29c49259
Anh Nguyen <drtannta@gmail.com>
2024-08-06 21:33:15 +0700

    reactivate c-j and c-k for cmp and telescope selection

--- lua/xubi/plugins/telescope.lua
+++ lua/xubi/plugins/telescope.lua
@@ -46,20 +46,20 @@ return {
 
     -- [[ Configure Telescope ]]
     -- See `:help telescope` and `:help telescope.setup()`
-    -- local actions = require 'telescope.actions'
+    local actions = require 'telescope.actions'
     require('telescope').setup {
       -- You can put your default mappings / updates / etc. in here
       --  All the info you're looking for is in `:help telescope.setup()`
       --
-      -- defaults = {
-      --   mappings = {
-      --     i = {
-      --       ['<c-enter>'] = 'to_fuzzy_refine',
-      --       ['<C-k>'] = actions.move_selection_previous,
-      --       ['<C-j>'] = actions.move_selection_next,
-      --     },
-      --   },
-      -- },
+      defaults = {
+        mappings = {
+          i = {
+            -- ['<c-enter>'] = 'to_fuzzy_refine',
+            ['<C-k>'] = actions.move_selection_previous,
+            ['<C-j>'] = actions.move_selection_next,
+          },
+        },
+      },
       -- pickers = {}
       extensions = {
         ['ui-select'] = {
