bb571dc83b148d9037d41803cb34ef9b205cbe5e
Anh Nguyen <drtannta@gmail.com>
2026-06-29 22:12:02 +0700

    fix client.supports_method is deprecated. Run ":checkhealth vim.deprecated" for more information

--- lua/xubi/lsp.lua
+++ lua/xubi/lsp.lua
@@ -65,7 +65,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     --
     -- This may be unwanted, since they displace some of your code
     local client = vim.lsp.get_client_by_id(event.data.client_id)
-    if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+    if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
       map('<leader>th', function()
         vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
       end, '[T]oggle Inlay [H]ints')
